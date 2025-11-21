d3dc8c06feae5c433eb141c6e525adcfb84a97fb
susu <tndus159200297@gmail.com>
2025-11-21 10:05:38 +0900

    member

---
+++ src/main/java/kr/co/wave/dto/config/TermsRepositoryDTO.java
@@ -0,0 +1,40 @@
+package kr.co.wave.dto.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TermsRepositoryDTO {
+
+    private int termsId;
+
+    private String type; // 유형
+
+    private String category;
+
+    private String title; // 제목
+
+    private String content; // 내용
+
+    private String version;	// 버전 (예: v1.0, v1.1)
+
+    private boolean isRequired; // 필수 여부
+
+    private LocalDate createdAt; // 생성일
+
+    private LocalDate updatedAt; // 수정일
+
+    private String pdfFile;
+
+    private String originalName;
+
+    private String termStatus;
+}
--- src/main/java/kr/co/wave/repository/member/signup.java
+++
@@ -1,9 +0,0 @@
-package kr.co.wave.repository.member;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface signup extends JpaRepository<signup,String> {
-    Optional<signup> findByMemId(String memId);
-}
